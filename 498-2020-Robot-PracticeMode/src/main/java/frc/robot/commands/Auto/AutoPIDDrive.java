c2be2aeea6305b53c3685b16342a62cc3e04eccd
Jack Haehl <jackeliash@gmail.com>
2020-03-10 17:22:36 -0700

    Initial Commit

---
+++ 498-2020-Robot-PracticeMode/src/main/java/frc/robot/commands/Auto/AutoPIDDrive.java
@@ -0,0 +1,25 @@
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class AutoPIDDrive extends CommandBase{
+    private final Drivetrain drivetrain;
+    private double distance;
+    public AutoPIDDrive(double distance){
+        this.distance = distance;
+    }
+
+    public void initialize(){
+        drivetrain.resetGyro();
+        drivetrain.setSetpoint(0);
+        drivetrain.enable();
+    }
+    public void execute(){
+
+    }
+    public void end(){
+        drivetrain.disable();
+    }
+    public boolean isFinished(){
+        return drivetrain.getTrueDistance() > distance;
+    }
+}
